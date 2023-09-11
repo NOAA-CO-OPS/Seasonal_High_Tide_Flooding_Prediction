b0cfffd58e990c17c88a51d35279b933bc630cee
Karen Kavanaugh <1398795926113302@mil>
2023-09-11 16:31:53 -0400

    Correction: concat table and then in separate if block replace NaNs with blanks. Need to test on all stations, but successfully tested on 3 stations.

--- main code functions/HTF_daily_predictions_all_stations.m
+++ main code functions/HTF_daily_predictions_all_stations.m
@@ -135,20 +135,21 @@ if ~isempty(find(strcmp(varargin, 'csv')))
     
     % concatenate all the tables inot one big table and write it to output
     % file
-    writetable(cat(1,all_data{:}),output_file);
+    %writetable(cat(1,all_data{:}),output_file);
+    writetable(cat(1,all_data{:}),output_file,'Delimiter', ',');
 
     % concatenate all the tables into one table without nans
-    %if true
-    %    writetable(all_data{ii},output_file,'Delimiter', ',');
-    %    fid = fopen(output_file,'rt');
-    %    X = fread(fid);
-    %    fclose(fid);
-    %    X = char(X.');
+    if true
+        %writetable(all_data{ii},output_file,'Delimiter', ',');
+        fid = fopen(output_file,'rt');
+        X = fread(fid);
+        fclose(fid);
+        X = char(X.');
         % replace string S1 with string S2
-    %    Y = strrep(X, 'NaN', '');
-    %    fid2 = fopen(output_file,'wt');
-    %    fwrite(fid2,Y);
-    %    fclose(fid2);
+        Y = strrep(X, 'NaN', '');
+        fid2 = fopen(output_file,'wt');
+        fwrite(fid2,Y);
+        fclose(fid2);
     end    
 
 
