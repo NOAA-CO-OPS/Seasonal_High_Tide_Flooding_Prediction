5f2e8a73159ce6682cf308fc757ee98c256f9a3f
Karen Kavanaugh <1398795926113302@mil>
2023-11-28 14:44:01 -0500

    Created function to retrieve flood levels and datums for a specified station and datum and then calcualte the NOS minor flood level relative to the specified datum and convert to meters.

---
+++ main code functions/getThresholddata.m
@@ -0,0 +1,43 @@
+function Output = getThresholddata(StationID,DatumBias)
+% Output = getThresholddata(StationID,DatumBias);
+%
+% This function downloads the NOS minor flood threshold on station datum (STND)
+% and specified datum bias for a given station from the CO-OPS API. It then
+% calculates the flood threshold for the specified datum. 
+% Required Inputs:
+% StationID > The station ID of the water level station of interest. 
+%             String. eg - '8452660'
+% DatumBias > The datum to which to bias the data to.  eg - 'mllw', 'mhw',
+%             etc. Options are 
+%             'MHHW' - mean higher high water
+%             'MHW' - mean high water
+%             'MTL' - mean tide level
+%             'MSL' - mean sea level
+%             'MLW' - mean low water
+%             'MLLW' - mean lower low water
+%             'NAVD' - North American Veritcal Datum of 1988
+%             'STND' - station datum
+
+% build url request for flood levels
+mdapi_url = "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/" + ...
+            "stations/";
+floodlevel_url = strcat(mdapi_url,StationID,...
+                        "/floodlevels.json");
+floodlevel = webread(floodlevel_url);
+% get NOS Minor flood threshold in feet relative to STND
+minor_floodlevel = floodlevel.nos_minor;
+
+% build url request for datums
+stn_datums_url = strcat(mdapi_url,StationID,...
+                   "/datums.json");
+stn_datums = webread(stn_datums_url);
+% get user selected datum in feet
+for i = 1:length(stn_datums.datums)
+    if stn_datums.datums(i).name == string(DatumBias)
+        stn_datum_bias = stn_datums.datums(i).value;
+        % Output = absolute value of minor_floodlevel - MHHW
+        floodlevel_datumbias = abs(minor_floodlevel-stn_datum_bias);
+        % convert from feet to meters and round to 3 decimals
+        Output = round(floodlevel_datumbias * unitsratio("meter","feet"),3);
+    end
+end
