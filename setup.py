bec49f6ef4370e8f79c1d6cc9b0c7bfb8bac4626
Matthew Conlin <1602604744113305@mil>
2024-08-23 09:25:30 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 15:24:02 2024
+
+@author: Matthew.Conlin
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='HTF',
+    version='1.0',
+    packages=find_packages(),
+    package_data={
+        'HTF': ['data/HighTideOutlookStationList_05_17_23_PAC_SLT.csv'],
+    },
+    include_package_data=True,
+    install_requires=['datetime','numpy','pandas',
+                      'pkg_resources','requests','scipy',
+                      'sklearn','warnings'],
+)
