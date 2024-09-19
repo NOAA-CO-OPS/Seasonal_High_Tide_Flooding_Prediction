4608fd9c90e554d14c01149faaec246f345181e9
Matthew Conlin <1602604744113305@mil>
2024-09-19 14:12:23 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HTF',
-    version='1.3.0',
+    version='1.3.1',
     packages=find_packages(),
     package_data={
         'HTF': ['data/HighTideOutlookStationList_05_17_23_PAC_SLT.csv'],
