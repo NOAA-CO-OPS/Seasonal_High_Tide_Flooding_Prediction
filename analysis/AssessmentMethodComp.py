8a2e6a51845a640792bdb533b392d0f919380635
Matthew Conlin <1602604744113305@mil>
2024-10-04 16:17:02 -0400

    Bump version

--- analysis/AssessmentMethodComp.py
+++ analysis/AssessmentMethodComp.py
@@ -6,41 +6,41 @@ Created on Mon Sep  9 14:17:12 2024
 """
 
 from HTF import HTF_model
-from matplotlib.patches import Rectangle
-import matplotlib.pyplot as plt
-import numpy as np
 
-model_dusek = HTF_model(8665530,
-                years_fit=[19830101,20011231],
-                years_assess=None,
-                years_pred=[20020101,20021231],
-                assess_method='DusekEtAl',
-                assess_metric='htf_days',
-                fold_size=1,
-                prctile_bin_val='pred_adj')
-model_dusek.train()
-model_dusek.assess()
+stations = [8670870,8665530,8661070,8658163,8651370]
+bss_dusek = []
+bss_xvalid = []
+for station in stations:
+    model_dusek = HTF_model(8665530,
+                    years_fit=[19830101,20011231],
+                    years_assess=[19830101,20011231],
+                    years_pred=None,
+                    assess_method='DusekEtAl',
+                    assess_metric='htf_days',
+                    fold_size=2,
+                    prctile_bin_val='pred_adj',
+                    cora_data_dir=None)
+    model_dusek.train()
+    model_dusek.assess()
 
-model_xvalid = HTF_model(8665530,
-                years_fit=[19830101,20011231],
-                years_assess=None,
-                years_pred=[20020101,20021231],
-                assess_method='cross_validation',
-                assess_metric='htf_days',
-                fold_size=2,
-                prctile_bin_val='pred_adj')
-model_xvalid.train()
-model_xvalid.assess()
 
-fig,ax = plt.subplots(1)
-ax.set_xlim(1983,2002)
-for i in range(len(model_xvalid.skill)):
-    r = Rectangle((1983+(2*i),model_xvalid.skill[i]['bss']),2,0,facecolor='k',edgecolor='k')
-    ax.add_patch(r)
-bss_all = np.array([model_xvalid.skill[i]['bss'] for i in range(len(model_xvalid.skill))])
-bss_mean = np.mean(bss_all[~np.isinf(bss_all)])
-ax.plot([1983,2002],[bss_mean,bss_mean],'k--')    
-ax.plot([1983,2002],[model_dusek.skill['bss'],model_dusek.skill['bss']],'r--')    
+    model_xvalid = HTF_model(8665530,
+                    years_fit=[19830101,20011231],
+                    years_assess=[19830101,20011231],
+                    years_pred=[20020101,20021231],
+                    assess_method='cross_validation',
+                    assess_metric='htf_days',
+                    fold_size=2,
+                    prctile_bin_val='pred_adj',
+                    cora_data_dir=None)
+    model_xvalid.train()
+    model_xvalid.assess()
+
+    skill_dusek = model_dusek.out_assess['f(lead)'].iloc[0]['out_assess']
+    skill_xvalid = model_xvalid.out_assess
+    
+    bss_dusek.append(skill_dusek['bss'])
+    bss_xvalid.append(skill_xvalid['bss'])
 
 
 
